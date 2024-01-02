b1df1ac36ac0c094f663efa4de991f9e2faad0fc
sibusisom.skhumkane@gmail.com <sibusisom.skhumkane@gmail.com>
2024-01-02 11:55:50 -0800

    Modified task1 and task6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
+
   for (let value of array) {
-    value = appendString + value;
-    array[idx] = value;
-    idx++;
+    array[idx++] = appendString + value;
   }
 
   return array;
