5051d01528d31c07e6b894c51ff714d1387929ee
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-01-04 14:33:26 -0800

    Modified Task5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,22 @@
-export default Building {
+export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building) {
       const props = Object.getOwnPropertyNames(this.constructor.prototype);
       if (!props.find((ev) => ev === 'evacuationWarningMessage')) {
-	throw new Error('Class extending Building must override evacuationWarningMessage');
+        throw new Error('Class extending Building must override evacuationWarningMessage');
       }
-    }
-    if ((typeof sqft !== 'number')) {
-      throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
   }
 }
