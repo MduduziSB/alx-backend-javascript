ed46d668cfecb3a5327a8b4c933b39e8c7f427b3
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-01-04 14:43:24 -0800

    modified Task6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,11 +1,8 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
     this._floors = floors;
   }
 
@@ -13,7 +10,14 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
+  set floors(floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = floors;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
