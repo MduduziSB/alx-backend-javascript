73dddcd16836e601b3d001286f82ab6387e15ca9
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-01-08 11:05:32 -0800

    Task0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,9 @@
 export default function getListStudents() {
-  const p1 = {id: 1, firstName: 'Guillaume', location: 'San Francisco'};
-  const p2 = {id: 2, firstName: 'James', location: 'Columbia'};
-  const p3 = {id: 5, firstName: 'Serena', location: 'San Francisco'};
-  return [p1, p2, p3];
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+
+  return students;
 }
