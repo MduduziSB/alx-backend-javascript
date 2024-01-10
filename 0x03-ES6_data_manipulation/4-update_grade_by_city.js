0588fb2b86fac35639558ccc2924be124e722ca3
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-01-10 03:14:32 -0800

    eslint compliant Task0 and Task4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,18 @@
 function updateStudentGradeByCity(students, city, newGrades) {
-    const sameCity = students.filter((student) => student.location === city);
-    return sameCity.map((student) => {
-      const updatedGrades = newGrades.filter((grade) => grade.studentId === student.id);
-      if (updatedGrades.length > 0) {
-        return {
-          ...student,
-          grade: updatedGrades[0].grade,
-        };
-      }
+  const sameCity = students.filter((student) => student.location === city);
+  return sameCity.map((student) => {
+    const updatedGrades = newGrades.filter((grade) => grade.studentId === student.id);
+    if (updatedGrades.length > 0) {
       return {
         ...student,
-        grade: 'N/A',
+        grade: updatedGrades[0].grade,
       };
-    });
-  }
-  
-  export default updateStudentGradeByCity;
+    }
+    return {
+      ...student,
+      grade: 'N/A',
+    };
+  });
+}
+
+export default updateStudentGradeByCity;
