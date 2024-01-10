f2b299b206cb66fbc8bbeab4e88edccc7c293a9e
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-01-09 18:51:38 -0800

    Corrected Task5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,13 @@
-function createInt8TypedArray(length, position, value) {
-    const arrayBuffer = new ArrayBuffer(length);
-    const int8Array = new Int8Array(arrayBuffer);
+const createInt8TypedArray = (length, position, value) => {
+  const arrayBuffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(arrayBuffer);
 
-    if (position >= int8Array.length || position < 0) {
-        throw new Error('Position outside range');
-    }
+  if (position >= int8Array.length || position < 0) {
+    throw new Error('Position outside range');
+  }
 
-    int8Array[position] = value;
-
-    return new DataView(arrayBuffer);
-}
+  int8Array[position] = value;
+  return new DataView(arrayBuffer);
+};
 
 export default createInt8TypedArray;
