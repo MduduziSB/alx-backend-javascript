97a5dfee87504ecb0c57f8ac1806e85e1ecbaa41
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-02-28 00:22:57 +0000

    Modified Task4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,19 @@
 const http = require('http');
 
 const PORT = 1245;
-const HOST = '127.0.0.1';
+const HOST = 'localhost';
 const app = http.createServer();
 
-app.on((req, res) => {
+// Attach a listener for the 'request' event
+app.on('request', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(PORT, HOST, () => {});
+app.listen(PORT, HOST, () => {
+  //console.log(`Server is running at http://${HOST}:${PORT}/`);
+});
 
 module.exports = app;
+
