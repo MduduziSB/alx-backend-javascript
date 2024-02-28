ad4aecb51522b60c4aee7372acb960d038386cad
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-02-28 00:31:19 +0000

    eslint compliant js files

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,16 +4,12 @@ const PORT = 1245;
 const HOST = 'localhost';
 const app = http.createServer();
 
-// Attach a listener for the 'request' event
 app.on('request', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(PORT, HOST, () => {
-  //console.log(`Server is running at http://${HOST}:${PORT}/`);
-});
+app.listen(PORT, HOST, () => {});
 
 module.exports = app;
-
