76d940c2bba765a0f74fd6a1c9761f3a8ee28ef9
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-02-29 22:21:36 +0000

    Task4

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi calls with correct arguments', () => {
+    const consoleLog = sinon.spy(Utils);
+    const calculateNumberStub = inon.stub(Utils, 'calculateNumber');
+
+    calculateNumberStub.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberStub.callCount).to.be.equal(1);
+    expect(consoleLog.log.calledWith('The total is: 10')).to.be.true;
+    expect(consoleLog.log.callCount).to.be.equal(1);
+    calculateNumberStub.restore();
+    consoleLog.log.restore();
+  });
+});
